cec181a18a7530660992f462fdb9958dbf962ca7
Jim Weirich <jim.weirich@gmail.com>
2013-08-17 19:14:07 -0400

    Creatio Ex Nihilo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friends::Application.config.secret_token = '7745a0d0d955c7219d6e7d800f7ac9f5d8ba7f5594df28e4f2e024349fdf445a5a0ee14869367a72bfc89a57b2c9f9aeddbc216a8f241981dbca1cc0f26da1c5'
